04e07464e69f05628ad4d41ad777e21b0955a34a
Foster Obeng Gyamfi <94114815+Phosters@users.noreply.github.com>
2023-09-25 13:19:42 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,35 @@
+#https://registry.terraform.io/providers/hashicorp/aws/latest
 terraform {
   required_providers {
     random = {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
 
+#https://registry.terraform.io/providers/hashicorp/random/latest
 provider "random" {
   # Configuration options
 }
 
 resource "random_string" "bucket_name" {
-  length           = 16
+  lower            = true
+  upper            = false
+  length           = 32
   special          = false
-  override_special = ""
+
 }
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "example" {
+  bucket = "random_string.bucket_name.result"
+}
+
 
 output "random_bucket_name_result" {
   value = random_string.bucket_name.result
