6a9023a901aeec940fcdb204662db0500da18f35
phoster <fgyamfiobeng5@gmail.com>
2023-10-02 15:12:23 +0000

    :muscle: # 33-aws-terrrahouse-module

--- main.tf
+++ main.tf
@@ -1,7 +1,25 @@
+#https://registry.terraform.io/providers/hashicorp/aws/latest
+terraform {
+  
+  cloud {
+    organization = "phosters"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
+
+
+}
+
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "website_bucket" {
   bucket = var.bucket_name
 }
 
 
-
+module "terrahouse_aws" {
+    source = "./modules/terrahouse_aws"
+    user_uuid = "var.user_uuid"
+    bucket_name = "var.bucket_name"
+}
