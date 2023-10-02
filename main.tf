6a857a3dfb0365b4b4e3b143a6fa3a6131c22379
phoster <fgyamfiobeng5@gmail.com>
2023-10-02 21:39:20 +0000

    update main.tf

--- main.tf
+++ main.tf
@@ -12,11 +12,6 @@ terraform {
 
 }
 
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.bucket_name
-}
-
 
 module "terrahouse_aws" {
     source = "./modules/terrahouse_aws"
--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -8,3 +8,8 @@ terraform {
   }
 }
 
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+}
