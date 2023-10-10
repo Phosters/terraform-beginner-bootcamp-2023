8161ca295d27e04fa010dbd5d87f51720663918c
Phosters <fgyamfiobeng5@gmail.com>
2023-10-10 13:18:43 +0000

    fixed cdn

--- modules/terrahouse_aws/resource-cdn.tf
+++ modules/terrahouse_aws/resource-cdn.tf
@@ -1,4 +1,4 @@
-# # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_origin_access_control
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_origin_access_control
 # resource "aws_cloudfront_origin_access_control" "default" {
 #   name  = "OAC${var.bucket_name}"
 #   description  = "Origin Access Control for static website hosting ${var.bucket_name}"
@@ -58,29 +58,7 @@
 #     UserUuid = var.user_uuid
 #   }
 
-
 #   viewer_certificate {
 #     cloudfront_default_certificate = true
 #   }
 # }
-
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-}
-
-
-resource "terraform_data" "invalidate_cache" {
-  triggers_replace = terraform_data.content_version.output
-
-  # https://developer.hashicorp.com/terraform/language/expressions/strings
-  provisioner "local-exec" {
-  # command = "aws_cloudfront create-invalidation --distribution-id ${aws_cloudfront_distribution.example.id} --paths '/*'"
-  command = <<COMMAND
-  aws cloudfront create-invalidation \
-  --distribution-id ${aws_cloudfront_distribution.s3_distribution.id} \
-  --paths '/*'
-COMMAND
-  }
-}
-  
