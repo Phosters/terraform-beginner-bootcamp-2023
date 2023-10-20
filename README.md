9adea0ea178bb4b36db96be6955e1d62cd78e1bf
phoster <fgyamfiobeng5@gmail.com>
2023-10-20 23:51:39 +0000

    :guard: #61 setup two towns

--- README.md
+++ README.md
@@ -5,6 +5,10 @@
 ## Weekly Journals
 - [Week 0 Journal](.journal/week0.md)
 
+- [Week 1 Journal](.journal/week1.md)
+
+- [Week 2 Journal](.journal/week2.md)
+
 ## Estras
 - [Github TOC Markdown](https://ecotrust-canada.github.io/markdown-toc/)
 
--- main.tf
+++ main.tf
@@ -8,14 +8,23 @@ terraform {
   }
 
 }
-   
-#   cloud {
-#     organization = "phosters"
 
-#     workspaces {
-#       name = "terra-house-1"
-#     }
-#   }
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "phosters"
+
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+   
+  cloud {
+    organization = "phosters"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
 
  }
 
@@ -25,25 +34,41 @@ provider "terratowns" {
   token = var.terratowns_access_token
 }
 
-
-module "terrahouse_aws" {
-  source              = "./modules/terrahouse_aws"
-  user_uuid           = var.teacherseat_user_uuid
-  bucket_name         = var.bucket_name
-  error_html_filepath = var.error_html_filepath
-  index_html_filepath = var.index_html_filepath
-  content_version     = var.content_version
-  assets_path = var.assets_path
+module "home_arcanum_hosting" {
+  source = "./modules/terrahome_aws"
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.arcanum.public_path
+  content_version = var.arcanum.content_version
 }
 
 resource "terratowns_home" "home" {
+  name = "How to play Arcanum in 2023!"
+  description = <<DESCRIPTION
+Arcanum is a game from 2001 that shipped with alot of bugs.
+Modders have removed all the originals making this game really fun
+to play (despite that old look graphics). This is my guide that will
+show you how to play arcanum without spoiling the plot.
+DESCRIPTION
+  domain_name = module.home_arcanum_hosting.domain_name
+  town = "missingo"
+  content_version = var.arcanum.content_version
+}
+
+module "home_payday_hosting" {
+  source = "./modules/terrahome_aws"
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.payday.public_path
+  content_version = var.payday.content_version
+}
+
+resource "terratowns_home" "home_payday" {
   name = "Making your Payday Bar"
   description = <<DESCRIPTION
 Since I really like Payday candy bars but they cost so much to import
 into Canada, I decided I would see how I could my own Paydays bars,
 and if they are most cost effective.
 DESCRIPTION
-  domain_name = "module.terrahouse_aws.cloudfront_url"
+  domain_name = module.home_payday_hosting.domain_name
   town = "missingo"
-  content_version = 1
+  content_version = var.payday.content_version
 }
